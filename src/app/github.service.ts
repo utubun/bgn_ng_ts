d2f56491145d5ee18ac6aeadcf28c52ab1eac80d
utubun <dmytro.strunin@gmail.com>
2021-04-06 09:12:58 +0200

    Added github.service.ts

---
+++ src/app/github.service.ts
@@ -0,0 +1,23 @@
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core'; 
+
+export interface GitHubUser{
+    html_url: string,
+    avatar_url: string,
+    login: string,
+    score: string,
+    items: any    
+}
+
+@Injectable() 
+export class GitHubService{
+    constructor(private _http: HttpClient){
+
+    }
+
+    getGitHubData(_searchTerm:string):Observable<GitHubUser>{
+      return this._http.get<GitHubUser>
+        ("https://api.github.com/search/users?q="+_searchTerm);                    
+    }
+}
