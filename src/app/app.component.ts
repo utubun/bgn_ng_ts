a1973b088563ef1640b154305a8275222606f6cb
utubun <dmytro.strunin@gmail.com>
2021-04-06 09:11:12 +0200

    GitHub rest API, Beginning Angular with Typescript, chapter 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { GitHubService } from './github.service'; 
+import { filter,debounceTime,distinctUntilChanged } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [GitHubService]
 })
 export class AppComponent {
-  title = 'github-rest-app';
-}
+  searchControl = new FormControl();  
+  isLoading = false;
+
+  users: any[] = [];  
+
+  constructor(private _githubService: GitHubService){
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+        .pipe(filter(text => text.length >= 3), debounceTime(400),distinctUntilChanged())                
+        .subscribe(value => {
+          this.isLoading = true; 
+          this._githubService.getGitHubData(value)
+                .subscribe(data => {
+                  this.isLoading = false;        
+                  this.users = data.items;                    
+          });                                  
+    });          
+  }
+} 
